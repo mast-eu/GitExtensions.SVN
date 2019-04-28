27bcbdebe12337964eb5330a13826cddcd682296
Martin Steinisch <martin.steinisch@gmail.com>
2019-04-28 23:10:14 +0200

    rename everything from "GitExtensions.EmptyPlugin" to "GitExtensions.SVN"

---
+++ src/GitExtensions.SVN/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using GitExtensions.SVN.Properties;
+using GitUIPluginInterfaces;
+using ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitExtensions.SVN
+{
+    /// <summary>
+    /// Git Extensions SVN plugin implementation.
+    /// </summary>
+    [Export(typeof(IGitPlugin))]
+    public class Plugin : GitPluginBase
+    {
+        public Plugin()
+        {
+            Name = "SVN";
+            Description = "SVN Plugin";
+            Icon = Resources.Icon;
+        }
+
+        public override bool Execute(GitUIEventArgs e)
+        {
+            MessageBox.Show(e.OwnerForm, "Hello from the SVN Plugin.", "Git Extensions");
+            return true;
+        }
+    }
+}
